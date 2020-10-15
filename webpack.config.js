14a2ab54d8e1bcbaf370788f4aa8182f4082eaea
hoshina <hoshina12345@gmail.com>
2020-10-15 10:08:25 +0900

    datachannel.htmlからコピー

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,16 @@
 const { WebpackPluginServe: Serve } = require('webpack-plugin-serve');
+const fs = require('fs');
 
 const serve = new Serve({
   host: '0.0.0.0',
   port: 5000,
   static: ['./'],
   open: true,
-  liveReload: false
+  liveReload: false,
+  https: {
+    key: fs.readFileSync('/home/pi/demoCA/private/ca.key'),
+    cert: fs.readFileSync('/home/pi/demoCA/ca.crt')
+  }
 });
 module.exports = {
   mode: 'development',
